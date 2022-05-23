71f766e9678c2e1eef53de29e97846a5fb3cd632
dlekveishvili <falenito13>
2022-05-23 12:14:49 +0400

    set headers in post request

--- controllers/upload.php
+++ controllers/upload.php
@@ -41,6 +41,24 @@ class upload {
         // header('Content-Type: application/json; charset=utf-8');
         // echo json_encode($_FILES);
         // exit;
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+            // you want to allow, and if so:
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 1000');
+        }
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+            }
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+                header("Access-Control-Allow-Headers: Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization");
+            }
+            exit(0);
+        }
         foreach ($request->files['image'] as $image) {
             if($newName) {
                 $exploded = explode(".", $image['name']);
